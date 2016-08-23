6c179c9670b1ef651a33a3a5334209a1048668b7
Mike Dilger <mike@optcomp.nz>
2016-08-23 16:04:24 +1200

    docs updated

--- implementors/core/iter/iterator/trait.Iterator.js
+++ implementors/core/iter/iterator/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["libc"] = [];
+implementors["libc"] = [];implementors["resolv"] = ["impl&lt;'a,&nbsp;T:&nbsp;<a class='trait' href='resolv/record/trait.RecordData.html' title='resolv::record::RecordData'>RecordData</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html' title='core::iter::iterator::Iterator'>Iterator</a> for <a class='struct' href='resolv/struct.RecordItems.html' title='resolv::RecordItems'>RecordItems</a>&lt;'a,&nbsp;T&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- resolv/sidebar-items.js
+++ resolv/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Class","DNS Class."],["ResolverOption","Options for the Resolver"],["Section",""]],"mod":[["error",""],["record",""]],"struct":[["Flags",""],["Resolver",""],["Response",""]]});
+initSidebarItems({"enum":[["Class","DNS Class."],["ResolverOption","Options for the Resolver"],["Section",""]],"mod":[["error",""],["record",""]],"struct":[["Flags",""],["RecordItems","An iterator to iterate through DNS records"],["Resolver",""],["Response",""]]});
